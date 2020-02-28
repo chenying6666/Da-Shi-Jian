7c118cbcc88122eaa6e0560b5b3fca386b83d472
chenying6666 <61459945+chenying6666@users.noreply.github.com>
2020-02-28 14:04:13 +0800

    登录和注册页面

---
+++ assets/js/login.js
@@ -0,0 +1,56 @@
+$(function () {
+    var form = layui.form
+    var leyer = layui.leyer
+    $('.link-log').on('click', function () {
+        $('.login-box').hide()
+        $('.Reg-box').show()
+    })
+    $('.link-reg').on('click', function () {
+        $('.Reg-box').hide()
+        $('.login-box').show()
+
+    })
+    form.verify({
+        pws: [/^[\S]{6,12}$/, '密码必须6到12位'],
+        sampas: function (value) {
+            var pwd = $('.Reg-box [name=password]').val()
+            if (value !== pwd) {
+                return '两次密码不一样'
+            }
+        }
+    })
+    $('#form-reg').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: 'http://www.liulongbin.top:3007/api/reguser',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功，请登录')
+                $('.link-reg').click()
+            }
+        })
+    })
+    $('#form-login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: 'http://www.liulongbin.top:3007/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token', 'res.token')
+                location.href = "/index.html"
+            }
+        })
+    })
+
+
+
+})
